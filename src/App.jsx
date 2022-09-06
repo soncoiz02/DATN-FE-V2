c3fbdbff25efbb04a87637cd756a8de8d0f06222
Bảo Sơn Trần <sontb@toprate.io>
2022-09-06 11:42:33 +0700

    feat: glass box components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Button, Container, Typography } from '@mui/material'
 import React from 'react'
 import { useForm } from 'react-hook-form'
 import * as yup from 'yup'
+import GlassBox from './components/GlassBox'
 import RHFProvider from './components/ReactHookForm/RHFProvider'
 import RHFTextField from './components/ReactHookForm/RHFTextField'
 
@@ -27,6 +28,9 @@ const App = () => {
   return (
     <Container>
       <Typography variant='h1'>dsdsadas</Typography>
+      <GlassBox opacity={0.5}>
+        <Typography variant='h2'>Alo</Typography>
+      </GlassBox>
       <RHFProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
         <RHFTextField name='name' label='Name' />
         <Button type='submit' variant='contained'>
---
+++ src/components/GlassBox.jsx
@@ -0,0 +1,16 @@
+import { Box, styled } from '@mui/material'
+import React from 'react'
+
+const GlassBox = ({ children, opacity }) => {
+  return <CustomBox opacity={opacity}>{children}</CustomBox>
+}
+
+const CustomBox = styled(Box)`
+  border-radius: 20px;
+  box-shadow: 0px 12px 24px -4px rgba(145, 158, 171, 0.12), 0px 0px 2px rgba(145, 158, 171, 0.2);
+  backdrop-filter: blur(10px);
+  background-color: rgba(255, 255, 255, ${(props) => props.opacity});
+  padding: 30px;
+`
+
+export default GlassBox
