71de40d5a5bc8a77d40dfab16a601cf37d437b5c
Bảo Sơn Trần <sontb@toprate.io>
2022-09-16 21:41:28 +0700

    feat: handle api render list service comment

--- src/sections/client/detail-services/RatedTab.jsx
+++ src/sections/client/detail-services/RatedTab.jsx
@@ -9,6 +9,7 @@ import {
   List,
   ListItem,
   ListItemAvatar,
+  Rating,
   Stack,
   styled,
   Typography,
@@ -19,6 +20,7 @@ import { useEffect } from 'react'
 import serviceApi from '../../../api/service'
 import ListStar from '../../../components/ListStar'
 import ModalRated from '../../../components/ModalRated'
+import calculateDayLeft from '../../../utils/calculateDayLeft'
 
 const RatedTab = ({ index, value, serviceId, ...other }) => {
   const [openModal, setOpenModal] = useState(false)
@@ -48,84 +50,86 @@ const RatedTab = ({ index, value, serviceId, ...other }) => {
       <Box>
         <Stack gap={4}>
           {serviceRated && (
-            <Stack
-              direction={{ xs: 'column', md: 'row' }}
-              alignItems='center'
-              justifyContent='center'
-              gap={{ xs: 1, md: 6 }}
-            >
+            <>
               <Stack
-                sx={{ height: '100%', width: { xs: '100%', md: 'calc(100% / 3)' } }}
-                gap={1}
-                justifyContent='center'
+                direction={{ xs: 'column', md: 'row' }}
                 alignItems='center'
+                justifyContent='center'
+                gap={{ xs: 1, md: 6 }}
               >
-                <Typography variant='body1'>Đánh giá trung bình</Typography>
-                <Stack direction='row' gap={1} alignItems='center'>
-                  <Stack direction='row' alignItems='center'>
-                    <PrimaryColorText>{serviceRated.avgRated}</PrimaryColorText>
-                    <Typography variant='subtitile1'>/5.0</Typography>
-                  </Stack>
-                  <Star sx={{ color: yellow[600], fontSize: '30px' }} />
-                </Stack>
-                <Typography
-                  variant='body1'
-                  sx={{ display: 'flex', gap: '10px', alignItems: 'center' }}
+                <Stack
+                  sx={{ height: '100%', width: { xs: '100%', md: 'calc(100% / 3)' } }}
+                  gap={1}
+                  justifyContent='center'
+                  alignItems='center'
                 >
-                  Tổng <PrimaryColorText>{serviceRated.total}</PrimaryColorText> đánh giá
-                </Typography>
-              </Stack>
-              <Divider orientation='vertical' variant='middle' flexItem />
-              <Stack gap={2} sx={{ width: { xs: '100%', md: 'calc(100% / 3)' } }}>
-                {serviceRated.detailRated.map((info) => (
-                  <Stack direction='row' gap={1.5} alignItems='center'>
+                  <Typography variant='body1'>Đánh giá trung bình</Typography>
+                  <Stack direction='row' gap={1} alignItems='center'>
                     <Stack direction='row' alignItems='center'>
-                      <Typography variant='subtitile1'>{info.star}</Typography>
-                      <Star sx={{ color: yellow[600], fontSize: '24px' }} />
+                      <PrimaryColorText>{serviceRated.avgRated}</PrimaryColorText>
+                      <Typography variant='subtitile1'>/5.0</Typography>
                     </Stack>
-                    <Box sx={{ width: '100%' }}>
-                      <CustomProgress variant='determinate' value={info.count * 10} />
-                    </Box>
-                    <Typography variant='subtitile1'>{info.count}</Typography>
+                    <Star sx={{ color: yellow[600], fontSize: '30px' }} />
                   </Stack>
-                ))}
-              </Stack>
-              <Divider orientation='vertical' variant='middle' flexItem />
-              <Stack
-                sx={{ height: '100%', width: { xs: '100%', md: 'calc(100% / 3)' } }}
-                justifyContent='center'
-                alignItems='center'
-              >
-                <CustomOutlineButton
-                  startIcon={<Edit />}
-                  variant='outlined'
-                  onClick={() => setOpenModal(true)}
-                >
-                  Đánh giá của bạn
-                </CustomOutlineButton>
-              </Stack>
-            </Stack>
-          )}
-          <List sx={{ width: '100%' }}>
-            <ListItem>
-              <Stack direction='row' gap={3}>
-                <ListItemAvatar>
-                  <Avatar sx={{ width: '50px', height: '50px' }} />
-                </ListItemAvatar>
-                <Stack gap={1}>
-                  <Stack gap={1} direction='row' alignItems='center'>
-                    <ListStar spacing={0} number={5} size={20} />
-                    <Typography variant='body2'>1 ngày trước</Typography>
-                  </Stack>
-                  <Typography variant='body1'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores necessitatibus
-                    aperiam, enim libero, aliquam ab quia porro dignissimos illum nesciunt autem
-                    voluptas dolorum excepturi aliquid est facilis veritatis ut! Assumenda.
+                  <Typography
+                    variant='body1'
+                    sx={{ display: 'flex', gap: '10px', alignItems: 'center' }}
+                  >
+                    Tổng <PrimaryColorText>{serviceRated.total}</PrimaryColorText> đánh giá
                   </Typography>
                 </Stack>
+                <Divider orientation='vertical' variant='middle' flexItem />
+                <Stack gap={2} sx={{ width: { xs: '100%', md: 'calc(100% / 3)' } }}>
+                  {serviceRated.detailRated.map((info) => (
+                    <Stack direction='row' gap={1.5} alignItems='center' key={info.star}>
+                      <Stack direction='row' alignItems='center'>
+                        <Typography variant='subtitile1'>{info.star}</Typography>
+                        <Star sx={{ color: yellow[600], fontSize: '24px' }} />
+                      </Stack>
+                      <Box sx={{ width: '100%' }}>
+                        <CustomProgress variant='determinate' value={info.count * 10} />
+                      </Box>
+                      <Typography variant='subtitile1'>{info.count}</Typography>
+                    </Stack>
+                  ))}
+                </Stack>
+                <Divider orientation='vertical' variant='middle' flexItem />
+                <Stack
+                  sx={{ height: '100%', width: { xs: '100%', md: 'calc(100% / 3)' } }}
+                  justifyContent='center'
+                  alignItems='center'
+                >
+                  <CustomOutlineButton
+                    startIcon={<Edit />}
+                    variant='outlined'
+                    onClick={() => setOpenModal(true)}
+                  >
+                    Đánh giá của bạn
+                  </CustomOutlineButton>
+                </Stack>
               </Stack>
-            </ListItem>
-          </List>
+              <List sx={{ width: '100%' }}>
+                <Stack gap={3}>
+                  {serviceRated.list.map((item) => (
+                    <Stack direction='row' gap={3} key={item._id}>
+                      <ListItemAvatar>
+                        <Avatar sx={{ width: '50px', height: '50px' }} />
+                      </ListItemAvatar>
+                      <Stack gap={1}>
+                        <Stack gap={1} direction='row' alignItems='center'>
+                          <Rating value={item.rate} readOnly />
+                          <Typography variant='body2'>
+                            {calculateDayLeft(item.createdAt)}
+                          </Typography>
+                        </Stack>
+                        <Typography variant='body1'>{item.content}</Typography>
+                      </Stack>
+                    </Stack>
+                  ))}
+                </Stack>
+              </List>
+            </>
+          )}
         </Stack>
         <ModalRated openModal={openModal} onCloseModal={() => setOpenModal(false)} />
       </Box>
