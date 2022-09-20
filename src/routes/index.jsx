982c7a61923a2e83029014643d5305999cf1a856
Le Duc Dung <dungld@newwave.vn>
2022-09-20 15:41:20 +0700

    fix: fix merge conflict

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,6 +5,7 @@ import ClientLayout from '../layouts/client/ClientLayout'
 import Dashboard from '../pages/admin/dashboard'
 import DetailService from '../pages/client/detail-service'
 import HomePage from '../pages/client/home'
+import ServiceRegister from '../pages/client/service-register-history'
 import Store from '../pages/client/store'
 const Router = () => {
   return (
@@ -13,6 +14,7 @@ const Router = () => {
         <Route index element={<HomePage />} />
         <Route path='store' element={<Store />} />
         <Route path='service/:id' element={<DetailService />} />
+        <Route path='service-register-history' element={<ServiceRegister />} />
       </Route>
       <Route path='/admin' element={<AdminLayout />}>
         <Route path='dashboard' element={<Dashboard />} />
