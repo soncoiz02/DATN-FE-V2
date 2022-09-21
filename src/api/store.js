5bd345082e21b835b3c0c2821e3a89b10c8dd29b
dung08072002 <dungnvph17931@fpt.edu.vn>
2022-09-21 11:27:04 +0700

    feat: api store

--- src/api/store.js
+++ src/api/store.js
@@ -2,7 +2,19 @@ import axiosClient from './axiosClient'
 
 const storeApi = {
   getAll() {
-    return axiosClient.get('/store')
+    return axiosClient.get(`/store`)
+  },
+  getOne(id) {
+    return axiosClient.get(`/store/${id}`)
+  },
+  create(data) {
+    return axiosClient.post(`/store`, data)
+  },
+  update(id, data) {
+    return axiosClient.put(`/store/${id}`, data)
+  },
+  remove(id) {
+    return axiosClient.delete(`store/${id}`)
   },
 }
 
--- src/pages/client/store/index.jsx
+++ src/pages/client/store/index.jsx
@@ -8,30 +8,20 @@ import FunctionBar from '../../../sections/client/store/FunctionBar'
 const Store = (props) => {
   return (
     <Box>
-      {/* Filter Mobile */}
       <FilterStoreMobile />
-      {/* End Filter Mobile */}
       <Grid container spacing={5}>
         <Grid item xs={12} sm={12} md={3} lg={3}>
-          {/* Filter */}
           <FilterStore />
         </Grid>
-        {/* MainContent */}
         <Grid item xs={12} sm={12} md={9} lg={9}>
           <Box sx={{ padding: { xs: '0 10px', md: '80px 20px' } }}>
-            {/* Search & Sort */}
             <FunctionBar />
-            {/* List Store */}
             <ListStore />
           </Box>
         </Grid>
       </Grid>
     </Box>
   )
-}
-
-const ResponsiveTitleCard = {
-  border: '',
 }
 
 export default Store
