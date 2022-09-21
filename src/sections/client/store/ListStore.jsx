97bcddeb842cc5ec0c05d88a8fc5e2dd18514a72
dung08072002 <dungnvph17931@fpt.edu.vn>
2022-09-21 17:04:11 +0700

    feat: call api to display list store

--- src/sections/client/store/ListStore.jsx
+++ src/sections/client/store/ListStore.jsx
@@ -35,106 +35,114 @@ const ListStore = () => {
   return (
     <Box sx={{ padding: { xs: '0', md: '0 50px' }, marginTop: '38px' }}>
       <Grid container spacing={{ xs: 1, md: 6 }}>
-        <Grid item xs={6} sm={6} md={6} lg={6}>
-          <Card
-            sx={{
-              maxWidth: 1,
-              position: 'relative',
-              boxShadow:
-                '0px 12px 24px -4px rgba(145, 158, 171, 0.12), 0px 0px 2px rgba(145, 158, 171, 0.2)',
-            }}
-          >
-            <CardMedia
-              component='img'
-              alt='green iguana'
-              height='140'
-              image='https://images.unsplash.com/photo-1553095066-5014bc7b7f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8d2FsbCUyMGJhY2tncm91bmR8ZW58MHx8MHx8&w=1000&q=80'
-            />
-            <CardContent sx={{ padding: { xs: '2px 15px 10px 15px', md: '4px 30px 30px 30px' } }}>
-              <Stack direction={{ xs: 'column', sm: 'column', md: 'row' }}>
-                {/* Avatar */}
-                <Box>
-                  <Avatar
+        {storeSpa.map((item, index) => (
+          <Grid item xs={6} sm={6} md={6} lg={6} key={index}>
+            <Card
+              sx={{
+                maxWidth: 1,
+                position: 'relative',
+                boxShadow:
+                  '0px 12px 24px -4px rgba(145, 158, 171, 0.12), 0px 0px 2px rgba(145, 158, 171, 0.2)',
+              }}
+            >
+              {/* Background */}
+              <CardMedia component='img' alt='green iguana' height='140' image={item.coverImg} />
+              <CardContent sx={{ padding: { xs: '2px 15px 10px 15px', md: '4px 30px 30px 30px' } }}>
+                <Stack direction={{ xs: 'column', sm: 'column', md: 'row' }}>
+                  {/* Avatar */}
+                  <Box>
+                    <Avatar
+                      sx={{
+                        width: 100,
+                        height: 100,
+                        position: 'absolute',
+                        top: { xs: 90, md: 110 },
+                        left: { xs: '50%', sm: '50%', md: 30 },
+                        transform: {
+                          xs: 'translateX(-50%)',
+                          sm: 'translateX(-50%)',
+                          md: 'none',
+                        },
+                      }}
+                      src={item.avt}
+                    />
+                  </Box>
+                  {/* Infor SPA */}
+                  <Box
                     sx={{
-                      width: 100,
-                      height: 100,
-                      position: 'absolute',
-                      top: { xs: 90, md: 110 },
-                      left: { xs: '50%', sm: '50%', md: 30 },
-                      transform: {
-                        xs: 'translateX(-50%)',
-                        sm: 'translateX(-50%)',
-                        md: 'none',
-                      },
+                      marginLeft: '132px',
+                      overflow: 'hidden',
+                      margin: { xs: '50px auto 0 auto', sm: '80px auto 0 auto', md: '0' },
+                      textAlign: { xs: 'center', sm: 'center', md: 'left' },
                     }}
-                    src='https://i.pinimg.com/236x/f8/28/f0/f828f043932924d093e3e6f2ef227535.jpg'
-                  />
-                </Box>
-                {/* Infor SPA */}
-                <Box
-                  sx={{
-                    marginLeft: '132px',
-                    overflow: 'hidden',
-                    margin: { xs: '50px auto 0 auto', sm: '80px auto 0 auto', md: '0' },
-                    textAlign: { xs: 'center', sm: 'center', md: 'left' },
-                  }}
-                >
-                  <Stack
-                    direction='column'
-                    justifyContent='center'
-                    alignItems='flex-start'
-                    spacing={0}
                   >
-                    <Typography
-                      variant='h3'
-                      sx={{ margin: { xs: '0 auto', sm: '0 auto', md: '0 0 0 132px' } }}
-                      color={theme.palette.text.secondary}
+                    <Stack
+                      direction='column'
+                      justifyContent='center'
+                      alignItems='flex-start'
+                      spacing={0}
                     >
-                      Excellence Spa Đỗ Quang
-                    </Typography>
-                    <Stack direction={{ xs: 'column-reverse', md: 'column' }}>
-                      <Rating
-                        name='size-small'
-                        sx={{ margin: { xs: '0 auto', md: '0 0 0 132px' } }}
-                        readOnly
-                        defaultValue={5}
-                      ></Rating>
-                      <Typography
-                        component='div'
-                        sx={{ margin: { xs: '0 auto', md: '0 0 0 132px' } }}
-                        variant='subtitle2'
+                      <StyleTypoNameSpa
+                        variant='h3'
+                        sx={{ margin: { xs: '0 auto', sm: '0 auto', md: '0 0 0 132px' } }}
+                        color={theme.palette.text.secondary}
                       >
-                        Địa chỉ: 138 Trần Bình, Mỹ Đình 2, Nam Từ Liêm, Hà Nội
-                      </Typography>
+                        {item.name}
+                      </StyleTypoNameSpa>
+                      <Stack direction={{ xs: 'column-reverse', md: 'column' }}>
+                        <Rating
+                          name='size-small'
+                          sx={{ margin: { xs: '0 auto', md: '0 0 0 132px' } }}
+                          readOnly
+                          defaultValue={5}
+                        ></Rating>
+                        <StyleTypoAddress
+                          sx={{ margin: { xs: '0 auto', md: '0 0 0 132px' } }}
+                          variant='subtitle2'
+                        >
+                          Địa chỉ: {item.address}
+                        </StyleTypoAddress>
+                      </Stack>
                     </Stack>
-                  </Stack>
+                  </Box>
+                </Stack>
+                <Box sx={{ marginTop: { xs: '10px', md: '20px' } }}>
+                  <StyleTypoDesc variant='subtitle2'>{item.desc}</StyleTypoDesc>
                 </Box>
-              </Stack>
-              <Box sx={{ marginTop: { xs: '10px', md: '20px' } }}>
-                <StyleTypography variant='subtitle2'>
-                  Excellence Spa - Chuyên gia chăm sóc sức khỏe dưỡng sinh thuần tự nhiên. Trong hơn
-                  19 năm qua, Excellence đã giúp hơn 285 ngàn người cải thiện sức khỏe. Được khách
-                  hàng đánh giá là một trong những SPA hạng nhất của Việt Nam.
-                </StyleTypography>
-              </Box>
-            </CardContent>
-            <CardActions sx={{ padding: { xs: '0 15px 15px 15px', md: '0 30px 30px 30px' } }}>
-              <Typography variant='subtitle2' color='#00BC6D'>
-                Đang mở cửa
-              </Typography>
-            </CardActions>
-          </Card>
-        </Grid>
+              </CardContent>
+              <CardActions sx={{ padding: { xs: '0 15px 15px 15px', md: '0 30px 30px 30px' } }}>
+                <Typography variant='subtitle2' color='#00BC6D'>
+                  Đang mở cửa
+                </Typography>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
       </Grid>
     </Box>
   )
 }
 
-const StyleTypography = styled(Typography)`
+const StyleTypoDesc = styled(Typography)`
   display: -webkit-box;
   overflow: hidden;
   text-overflow: ellipsis;
   -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+`
+
+const StyleTypoAddress = styled(Typography)`
+  display: -webkit-box;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+`
+const StyleTypoNameSpa = styled(Typography)`
+  display: -webkit-box;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  -webkit-line-clamp: 2;
   -webkit-box-orient: vertical;
 `
 
