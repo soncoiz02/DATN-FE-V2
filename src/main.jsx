73c48dcea3419e92798c6d5573eb0708069c5873
Bảo Sơn Trần <sontb@toprate.io>
2022-09-23 10:27:04 +0700

    chore: config redux toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,19 @@ import App from './App'
 import MuiThemeProvider from './themes/MuiThemeProvider'
 import { BrowserRouter } from 'react-router-dom'
 import ScrollToTop from './components/ScrollToTop'
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <MuiThemeProvider>
-        <ScrollToTop />
-        <App />
-      </MuiThemeProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <MuiThemeProvider>
+          <ScrollToTop />
+          <App />
+        </MuiThemeProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 )
---
+++ src/redux/slice/serviceRegisterSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  list: [],
+}
+
+export const serviceRegisterSlice = createSlice({
+  name: 'serviceRegister',
+  initialState,
+  reducers: {
+    getFullList: (state, action) => {
+      state.list = action.payload
+    },
+  },
+})
+
+export const { getFullList } = counterSlice.actions
+export default serviceRegisterSlice.reducer
