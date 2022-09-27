215757ad39ba3290fb910e59d4f3f9df5fc211e5
Le Duc Dung <dungld@newwave.vn>
2022-09-27 15:53:35 +0700

    feat: api filter service by status

--- src/api/order.js
+++ src/api/order.js
@@ -4,6 +4,10 @@ const orderApi = {
   getAll() {
     return axiosClient.get('/order')
   },
+
+  getByStatus(type) {
+    return axiosClient.get(`/filterByStatus?status=${type}`)
+  },
 }
 
 export default orderApi
